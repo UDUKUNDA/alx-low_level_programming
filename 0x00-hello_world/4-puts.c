2d2fef093a6827a8574eb7cdd98614a9cea32ff8
UDUKUNDA <udukundaelysee@gmail.com>
2023-03-17 05:21:25 -0700

    hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry  point
+ * puts - Prints the output on
+ * on the screen.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+
+	return (0);
+}
