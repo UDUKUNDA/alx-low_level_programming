4f5375138662c3d03ab45a97bd321d887d8474d6
UDUKUNDA <udukundaelysee@gmail.com>
2023-03-18 10:26:18 -0700

    hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * putchar - Prints characters
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char c;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar('0' + n);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+		putchar('\n');
+	return (0);
+}
