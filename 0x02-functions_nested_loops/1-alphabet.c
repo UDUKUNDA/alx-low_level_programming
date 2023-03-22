01ea60f013fc590646f12d42d2202e305b6b6d28
UDUKUNDA <udukundaelysee@gmail.com>
2023-03-22 14:38:47 +0000

    play alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,12 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * _puchar - to print a character
  * print_alphabet - This function will print alphabets in lowercase
  *
- * Return: Always 0 (Success)
+ * Return: nothing  (Success)
  */
 
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-/**
- * print_alphabet - displays alphabet in lowercase
- *
- * Return: nothing (Success)
- */
+
 void print_alphabet(void)
 {
 	char a;
