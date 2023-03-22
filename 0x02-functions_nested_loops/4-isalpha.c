afba08dd60ac599ec6fea1261bf81cd7a1a5030e
UDUKUNDA <udukunda@gmail.com>
2023-03-22 19:32:04 +0000

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+/**
+ * _isalpha - checks if an char is alphabet
+ * @c: argument to be checked
+ *
+ * Return: a value on Succes
+ */
+
+int _isalpha(int c)
+{
+	if (_isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
