c10dd23bd4093b1debb971b03d6f0f1dd95a8001
UDUKUNDA <udukunda@gmail.com>
2023-03-23 01:13:28 +0000

    timetables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,10 +9,11 @@
 
 void times_table(void)
 {
-	int i,j,m,n,x;
+	int i, j, m, n, x;
+
 	for (i = 0; i < 10; i++)
 	{
-	for(j = 0; j < 10; j++)
+	for (j = 0; j < 10; j++)
 	{
 		m = i * j;
 	if (m > 9)
@@ -26,7 +27,7 @@ void times_table(void)
 	}
 	else
 	{
-	if ( j != 0)
+	if (j != 0)
 	{
 		_putchar(44);
 		_putchar(32);
