19f1956539e461ea0982e46dcfd8fbd8a2c4c509
UDUKUNDA <udukunda@gmail.com>
2023-03-23 01:28:14 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - will add twoo numbers
+ * @a: passed argument
+ * @b: passed argument
+ *
+ * Return: value on success
+ */
+int add(int a, int b)
+{
+	int z = a + b;
+
+	return (z);
+}
