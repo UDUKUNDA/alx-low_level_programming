05472bc69e173930cc911806b593a6da7d150904
UDUKUNDA <udukunda@gmail.com>
2023-03-23 02:50:47 +0000

    nature

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int x, z = 0;
+
+	while (x < 1024)
+	{
+	if ((x % 3 == 0) || (x % 5 == 0))
+	{
+		z = z + 1;
+	}
+	x++;
+	}
+	printf("%d \n", z);
+	return (0);
+}
