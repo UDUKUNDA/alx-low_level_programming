79aa49a17d28aa3db61eebcc271dcffce6ac3c3b
UDUKUNDA <udukunda@gmail.com>
2023-03-23 11:06:35 +0000

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - to multiply
+ * @a: passed argument
+ * @b: passed argument
+ *
+ * Return: a value upon success
+ */
+
+int mul(int a, int b)
+{
+	int x = a * b;
+
+	return (x);
+}
