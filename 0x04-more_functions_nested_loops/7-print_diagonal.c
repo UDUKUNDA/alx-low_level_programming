fccabc09e30bc447b8324d117741b13bf880849e
UDUKUNDA <udukunda@gmail.com>
2023-03-23 15:45:55 +0000

    aline

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal line
+ * @n: passed aegument
+ *
+ * Return: 0 upon success
+ */
+
+void print_diagonal(int n)
+{
+	int c, i;
+	
+	c = 0;
+
+	while (n > 0)
+	{
+		i = c;
+		while (i > 0)
+		{
+			_putchar(' ');
+			i--;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		c++;
+		n--;
+	}
+	if (c < 1)
+
+		_putchar('\n');
+}
