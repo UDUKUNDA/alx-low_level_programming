4b8ee24c72bde042bc674dc6c0946c7494ba28e7
UDUKUNDA <udukunda@gmail.com>
2023-03-27 23:36:39 +0000

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this will update the vale
+ * @n: will be a pointer to integer
+ *
+ * Return: nothing upon success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This is a readme file of the current project 
+that on pointers and arrays and strings to.
