78ce56f2b4c6acfd6d0041db762d148b17615d85
UDUKUNDA <udukunda@gmail.com>
2023-03-28 01:06:00 +0000

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _puts - will print the string to screen
+ * @str: a pointer to the string
+ *
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
