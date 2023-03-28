8349ecde7abaf371da849eb46a9bc933885a885b
UDUKUNDA <udukunda@gmail.com>
2023-03-28 11:42:21 +0000

    array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - to print array element
+ * @a: an array of elements
+ * @n: size of the array
+ *
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+	if (a[i] != a[n-1])
+	{
+		printf(",");
+		printf(" ");
+	}
+	}
+	printf("\n");
+}
