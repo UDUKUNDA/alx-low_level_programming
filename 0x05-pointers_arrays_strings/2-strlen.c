42dc3bbd3a15ef10f2918023cf09c1e858327cf4
UDUKUNDA <udukunda@gmail.com>
2023-03-28 00:35:24 +0000

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - to return the length of the string
+ * @s:a poiinter to string
+ *
+ * Return: 0 upon success
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
