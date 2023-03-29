711f321069166472ef2fc0a0ee417d8f793697fd
UDUKUNDA <udukunda@gmail.com>
2023-03-29 17:16:04 +0000

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+/**
+ * string_toupper - converts string to uppercase
+ * @str: char type parametrr
+ * Return: a value
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+	if(str[i] > 'a' && str[i] > 'z')
+	{
+		str[i] = str[i] - 32;
+	}
+	i++;
+	}
+	return (str);
+}
