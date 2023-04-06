25f44cba84c24d12bed110ae6b87d58e67c95721
UDUKUNDA <udukunda@gmail.com>
2023-04-06 00:47:12 +0000

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen_recursion - return the length of a string
+ *@s: a str to pass
+ * Return: a value
+ */
+int _strlen_recursion(char *s)
+{
+	int n;
+	return (n = strlen(s));
+}
