8fe60ab81d23ff5b4e97dc5f4af613d6cf9501f8
UDUKUNDA <udukunda@gmail.com>
2023-04-06 02:23:43 +0000

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,12 +8,14 @@
  */
 int _pow_recursion(int x, int y)
 {
-	int n;
-
 	if (y < 0)
 	{
 		return (-1);
 	}
-	return (n = pow(x, y));
+	if (y == 0)
+	{
+		return (1);
+	}
+	return  (x * _pow_recursion(x, y - 1));
 }
 
