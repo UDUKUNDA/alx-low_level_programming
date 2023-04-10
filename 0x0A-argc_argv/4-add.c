e01217cfd7fff2474a5bd2a0f08b243ae2220d3f
UDUKUNDA <udukunda@gmail.com>
2023-04-10 21:53:31 +0000

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - adds number
+ * @argc: counts the number of args
+ * @argv: a pointer to array of string arg
+ *
+ * Return: a value
+ */
+int main(int argc, char **argv)
+{
+	int n, sum = 0, b = 0;
+
+	if (argc > 1)
+	{
+		for (n = 1; n < argc; n++)
+		{
+			if (isdigit(*argv[n]))
+			{
+			sum += atoi(argv[n]);
+			}
+			else
+			{
+			printf("Error\n");
+			return (1);
+			}
+
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("%d\n", b);
+	}
+	return (0);
+}
