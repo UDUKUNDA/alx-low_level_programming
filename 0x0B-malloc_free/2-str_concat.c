6efb09332901e88dace7227082673334a04c7ef1
UDUKUNDA <udukunda@gmail.com>
2023-04-12 02:47:16 +0000

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,31 +1,43 @@
 #include "main.h"
 #include <string.h>
 #include <stdlib.h>
+
 /**
- * str_concat - concatenantes string
+ * str_concat - cancates a  string
  * @s1: string one
  * @s2: string two
  *
- * Returns: a pointer to a string
+ * Return: a pointer to a string
  */
 char *str_concat(char *s1, char *s2)
 {
 	char *str;
 
-	if (s1 == NULL || s2 == NULL)
-	{
-		return (NULL);
-	}
 	str = malloc(strlen(s1) + strlen(s2) + 1);
 	if (str == NULL)
 	{
 		return (NULL);
 	}
+	if (s1 == NULL)
+	{
+		strcpy(str, " ");
+		strcat(str, s2);
+	}
+	else if (s2 == NULL)
+	{
+		strcpy(str, s1);
+		strcat(str, " ");
+	}
+	else if (s1 == NULL && s2 == NULL)
+	{
+		strcpy(str, " ");
+		strcat(str, " ");
+	}
+	else
+	{
 	strcpy(str, s1);
 	strcat(str, s2);
+	}
 	return (str);
 	free(str);
 }
-
-
-
