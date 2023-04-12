fcacf14ca60adc14f935bcf9c9c63c83c6dbd69d
UDUKUNDA <udukunda@gmail.com>
2023-04-12 00:32:32 +0000

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * create_array -this creates array of characters
+ * @size: the size of array
+ * @c: the character to initialize
+ * Return: a value
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else if (size > 0)
+	{
+	char *arr = (char *)malloc(size * sizeof(char));
+
+	for (i = 0; i < size; i++)
+	{
+	arr[i] = c;
+	}
+	return (arr);
+	free(arr);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+Okay this readme file here os going to be all about the dynamic memory allocation
+yeah
