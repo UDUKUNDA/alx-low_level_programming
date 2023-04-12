a947168bf8599f0ef5e8b5c6760f03a8860feaf8
UDUKUNDA <udukunda@gmail.com>
2023-04-12 00:49:54 +0000

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,6 +10,7 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
+	char *arr;
 
 	if (size == 0)
 	{
@@ -17,8 +18,7 @@ char *create_array(unsigned int size, char c)
 	}
 	else if (size > 0)
 	{
-	char *arr = (char*)malloc(size * sizeof(char));
-
+	arr = malloc(size * sizeof(char));
 	for (i = 0; i < size; i++)
 	{
 	arr[i] = c;
