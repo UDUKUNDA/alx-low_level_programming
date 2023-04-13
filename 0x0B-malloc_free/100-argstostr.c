cca28fa2b8f92246b2486bf2ad3328584e11866a
UDUKUNDA <udukunda@gmail.com>
2023-04-13 17:57:18 +0000

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -15,28 +15,27 @@ char *argstostr(int ac, char **av)
 	int len = 0, x = 0;
 	char *str;
 
- if (ac == 0 || av == NULL) {
-        return NULL;
-    }
-
-    for (i = 0; i < ac; i++) {
-        len += strlen(av[i]) + 1;
-    }
-
-    str = malloc(len * sizeof(char) + 1);
-    if (str == NULL) {
-        return NULL;
-    }
-
-    
-    for (i = 0; i < ac; i++) {
-        strcpy(&str[x], av[i]);
-        x += strlen(av[i]);
-        str[x] = '\n';
-        x++;
-    } 
-
-    return str;
-    free(str);
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		len += strlen(av[i]) + 1;
+	}
+	str = malloc(len * sizeof(char) + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		strcpy(&str[x], av[i]);
+		x += strlen(av[i]);
+		str[x] = '\n';
+		x++
+	}
+	return (str);
+	free(str);
 }
 
