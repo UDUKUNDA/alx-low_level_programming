d9726974970b8b9864b399348589efd27c357194
UDUKUNDA <udukunda@gmail.com>
2023-04-14 11:44:59 +0000

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * _calloc - allocate memo for ana array
  * @nmemb: the n element
@@ -8,13 +9,13 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *str;
+	void *str;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	str = malloc(nmemb * size);
+	str = calloc(nmemb, size);
 	if (str == NULL)
 	{
 		return (NULL);
