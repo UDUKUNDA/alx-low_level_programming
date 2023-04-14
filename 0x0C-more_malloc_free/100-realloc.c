acd55b907c3f820c3796ec9cb44ede55829a786f
UDUKUNDA <udukunda@gmail.com>
2023-04-14 14:51:29 +0000

    100-realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,10 +12,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
 
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
+	}
+	if (new_size == old_size)
+	{
+		return (ptr);
 	}
 	new_ptr = malloc(new_size);
 	if (ptr == NULL)
