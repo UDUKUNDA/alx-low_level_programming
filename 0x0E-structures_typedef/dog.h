b909966ff6e01184cfb226d1706c2cfd54a75a8b
UDUKUNDA <udukundaelysee@gmail.com>
2023-04-18 01:14:34 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog attributes
+ * @name: its name
+ * @age: the age
+ * @owner: the dog owner
+ *
+ * Description: I don't have any longer description
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
