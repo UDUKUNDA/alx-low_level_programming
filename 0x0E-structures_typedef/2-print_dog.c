487f26767d2713ce9898e1415e5f135a64322140
UDUKUNDA <udukundaelysee@gmail.com>
2023-04-18 02:18:16 +0000

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,17 +10,23 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	{
-		printf(" ");
+		return;
 	}
 	if (d->name == NULL)
 	{
 		printf("Name: (nil)");
 	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 	{
 		printf("Owner: (nil)");
 	}
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
 }
