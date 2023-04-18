4317964aecc09d49372b6897315f4703cef0a9c7
UDUKUNDA <udukundaelysee@gmail.com>
2023-04-18 02:24:16 +0000

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,7 +13,7 @@ typedef struct dog_t
 	char *name;
 	char *owner;
 	float age;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
