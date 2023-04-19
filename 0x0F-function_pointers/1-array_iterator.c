f64ebe96f4e8a2a756ec5d80e8f2f00118be682c
UDUKUNDA <udukundaelysee@gmail.com>
2023-04-19 17:02:48 +0000

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * array_iterator - iterates an array
+ * @array: a passed array
+ * @size: the size of array
+ * @action: a function to execute
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
