dd010a918a3fe6a01ec66098a282324a45ef36b8
UDUKUNDA <udukundaelysee@gmail.com>
2023-04-20 02:52:22 +0000

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,35 +9,37 @@ void print_opcodes(char *start, int num_bytes);
  */
 int main(int argc, char **argv)
 {
-    if (argc != 2) {
-        printf("Error\n");
-        return 1;
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	int nb = atoi(argv[1]);
 
-    int num_bytes = atoi(argv[1]);
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	char *s = (char *) &main;
 
-    if (num_bytes < 0) {
-        printf("Error\n");
-        return 2;
-    }
-
-    char *start = (char*) &main;
-    print_opcodes(start, num_bytes);
-
-    return 0;
+	print_opcodes(s, nb);
+	return (0);
 }
 /**
  * print_opcodes - display the main's opcode
- * @start: value to begin
- * @num_bytes: to end
+ * @s: value to begin
+ * @nb: to end
  * Return: nothing
  */
-void print_opcodes(char *start, int num_bytes)
+void print_opcodes(char *s, int nb)
 {
 	int i;
-    for (i = 0; i < num_bytes; i++) {
-        printf("%02hhx ", start[i]);
-    }
-    printf("\n");
+
+	for (i = 0; i < nb; i++)
+	{
+		printf("%02hhx ", start[i]);
+	}
+	printf("\n");
 }
 
