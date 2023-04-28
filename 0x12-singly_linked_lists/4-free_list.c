23082cc740fc3de631b265c340ef8bca6e8ea33f
UDUKUNDA <udukundaelysee@gmail.com>
2023-04-28 11:10:25 +0000

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_list - frees the list
+ * @head: pointer to the head of the list
+ *
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *cNode;
+
+	while (head != NULL)
+	{
+		cNode = head;
+		head = head->next;
+		free (cNode);
+	}
+}
