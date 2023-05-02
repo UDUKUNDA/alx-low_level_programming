2c97d5cf20d06be1094199afe8d0fe3d741ea86b
UDUKUNDA <udukundaelysee@gmail.com>
2023-05-02 09:40:44 +0000

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - frees the list
+ * @head: the pointer to the first element of the list
+ * Return: a value
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *it;
+
+	temp = *head;
+	while (temp != NULL)
+	{
+		it = temp->next;
+		free(temp);
+		temp = it;
+	}
+	*head = NULL;
+}
