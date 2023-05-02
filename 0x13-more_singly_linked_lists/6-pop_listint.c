70f481446bb6d42169c5ed3fc4c7f7bc3b1b7159
UDUKUNDA <udukundaelysee@gmail.com>
2023-05-02 10:48:37 +0000

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -13,11 +13,8 @@ int pop_listint(listint_t **head)
 	{
 		return (0);
 	}
-	else
-	{
-		temp = *head;
-		*head = temp->next;
-	}
+	temp = *head;
+	*head = (*head)->next;
 	return (temp->n);
 	free(temp);
 }
