19121aab0e6301c46f51b30590db6d703a864025
UDUKUNDA <udukundaelysee@gmail.com>
2023-05-03 01:30:16 +0000

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,8 +34,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	temp = *head;
 	while (temp != NULL && i < idx - 1)
 	{
-		i++;
 		temp = temp->next;
+		i++;
 	}
 	if (temp == NULL)
 	{
@@ -44,5 +44,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	Nnode->next = temp->next;
 	temp->next = Nnode;
-	return (NULL);
+	return (Nnode);
 }
