39447a38654dd57aa851ddd73adc1969daea54e8
UDUKUNDA <udukundaelysee@gmail.com>
2023-05-04 02:06:39 +0000

    102-free_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint_safe - this frees the loop function
+ * @h: the pointer to the head of the list
+ * Return: a value
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t i = 0;
+	listint_t *front = *h;
+	listint_t *behind = *h;
+	listint_t *temp;
+
+	while (front != NULL && front->next != NULL)
+	{
+		behind = behind->next;
+		front = front->next->next;
+		if (behind == front)
+		{
+			behind = *h;
+			while (behind != front)
+			{
+				temp = behind;
+				behind = behind->next;
+				free(temp);
+				i++;
+			}
+			while (front->next != behind)
+			{
+				temp = front;
+				front = front->next;
+				free(temp);
+				i++;
+			}
+			temp = front;
+			front = front->next;
+			free(temp);
+			i++;
+			break;
+		}
+	}
+	while (*h != NULL)
+	{
+		temp = *h;
+		*h = (*h)->next;
+		free(temp);
+		i++;
+	}
+	*h = NULL;
+	return (i);
+}
+
+
