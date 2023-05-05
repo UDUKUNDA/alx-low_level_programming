1a293b8beac84e153999928c515a7aaf4cf79e12
UDUKUNDA <udukundaelysee@gmail.com>
2023-05-05 16:53:51 +0000

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
+int power(int len);
 /**
  * binary_to_uint - converts a binary to ui;
  * @b: a binary number string
@@ -9,29 +10,49 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int power = 0;
-	int sum = 0, i, n;
-	int len = strlen(b);
-
-	if (b == NULL)
+	int len1 = strlen(b), i;
+	int len2 = len1, po, pd, x = 0;
+	unsigned int sum = 0;
+	
+	if ( b == NULL)
 	{
 		return (0);
 	}
-	for (i = len - 1; i >= 0; i--)
+	for (i = 0; i < len1; i++)
 	{
-		if (b[i] == '1')
+		if (b[i] != '0' && b[i] != '1')
 		{
-			sum += (int) pow(2, power);
+			x++;
 		}
-		power++;
-		if (b[i] != '0' || b[i] != '1')
-		{
-			n = 1;
-		}
+		po = power(len2);
+		pd = b[i] - '0';
+		sum += po * pd;
+		len2--;
 	}
-	if (n != 1)
+	if (x != 0)
 	{
 		return (0);
 	}
 	return (sum);
+}
+/**
+ * power - to give the powers of a binary number
+ * @len: passed string length
+ * Return: a value
+ */
+int power(int len)
+{
+	int prod =  1, i;
+	for (i = len - 1; i > 0; i--)
+	{
+		if (i == 0)
+		{
+			prod = 1;
+		}
+		else
+		{
+			prod *= 2;
+		}
+	}
+	return (prod);
 }
