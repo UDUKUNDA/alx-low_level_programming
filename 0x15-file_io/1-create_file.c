9a4f9d16fc2e72282003455f69b85358143bbdbd
UDUKUNDA <udukundaelysee@gmail.com>
2023-05-12 02:10:09 +0000

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (0);
 	}
-	fp = open("filename", O_CREAT | O_TRUNC | O_RDWR);
+	fp = open("filename", O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
 	if (fp == -1)
 	{
 		return (-1);
