3c7541ba19f41ef9d1da5d2f7d3ef5956cec4a61
UDUKUNDA <udukundaelysee@gmail.com>
2023-07-12 01:46:46 +0000

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * flip_bits - flips the bits and returns thier count
+ * @n: para 1
+ * @m: para 2
+ * Return:  a value
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+        unsigned int flipIt = 0;
+        unsigned int x;
+
+        x = n ^ m;
+        while (x)
+        {
+                x = x & (x - 1);
+                flipIt++;
+        }
+        return (flipIt);
+}
