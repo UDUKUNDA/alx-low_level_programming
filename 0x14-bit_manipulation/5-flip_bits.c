233110c4f81459c644fe225df9d54e668ea7ffe0
UDUKUNDA <udukundaelysee@gmail.com>
2023-07-12 01:49:54 +0000

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,14 +8,14 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-        unsigned int flipIt = 0;
-        unsigned int x;
+	unsigned int flipIt = 0;
+	unsigned int x;
 
-        x = n ^ m;
-        while (x)
-        {
-                x = x & (x - 1);
-                flipIt++;
-        }
-        return (flipIt);
+	x = n ^ m;
+	while (x)
+	{
+		x = x & (x - 1);
+		flipIt++;
+	}
+	return (flipIt);
 }
