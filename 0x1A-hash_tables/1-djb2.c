e170b3faa5ffe650c04cbcab44a088f977c00293
UDUKUNDA <udukudndaelysee@gmail.com>
2023-09-14 03:21:37 +0000

    hash tables

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Hash function implementing the djb2.
+ * @str: The string to hash.
+ *
+ * Return: The calculated hash.
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hsh;
+	int c;
+
+	hsh = 5381;
+	while ((c = *str++))
+		hsh = ((hsh << 5) + hsh) + c;
+
+	return (hsh);
+}
